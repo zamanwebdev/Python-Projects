efcf383127d281b3a1c09324ff59c92d9dfefcbf
Syed Zaman <zamanwebdev@gmail.com>
2026-02-07 00:19:27 +0600

    #2 Python Mad Libs Game Practice Done.

---
+++ MadLibsGame/MadLibsGame.py
@@ -0,0 +1,7 @@
+userName = input("What is your Name ? ")
+fatherName = input("What is your Father Name ? ")
+motherName = input("What is your Mother Name ? ")
+study = input("Witch class do you read in ? ")
+relationship = input("Tell me about your relationship ? ")
+print(f"My Friend's name is {userName}, My Friend's Father's name is {fatherName}, My Friend's Mother's name is {motherName}"
+      f"My Friend's Study in {study}, My Friend's relationship is {relationship}")
